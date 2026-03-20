e6d2d7b805a449c8a99ed144e9e12c238ccc87d7
weixiuzhisiku <bamaxiaofeng@outlook.com>
2026-03-20 11:26:29 +0800

    Delete new-case.sh

--- new-case.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-cd ~/repair-wiki/tiddlers
-echo "=== 新建维修案例 ==="
-read -p "品牌: " brand
-read -p "机型: " model
-read -p "故障: " fault
-echo "$brand-$model-$fault" > "$brand-$model-$fault.tid"
